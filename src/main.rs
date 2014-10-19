d20ad0a238ce659f438dd26d1cccbf45ca9f300e
Alexey Fedorov <waterlink000@gmail.com>
2014-10-20 02:41:43 +0300

    support PORT for heroku

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 extern crate john;
 
+use std::os::getenv;
+use std::io::net::ip::Port;
+
+#[cfg(not(test))]
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn get_server_port() -> Port {
+    getenv("PORT")
+        .and_then(|s| from_str::<Port>(s.as_slice()))
+        .unwrap_or(3000)
+}
+
 #[cfg(not(test))]
 fn main() {
-    john::Server::new(3100).start()
+    john::Server::new(get_server_port()).start()
 }
